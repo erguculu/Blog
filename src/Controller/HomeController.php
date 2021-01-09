348c726d132287899f309927df8aea0599bfe141
erguculu <erguculu@gmail.com>
2021-01-09 23:47:19 +0100

    Aded style and data fixturs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,28 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+ /**
+ * @Route("/", name="post_")
+ */
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
+     * @return Response a response instance
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        $post = new Post();
-        $post->setTitle('Titre 1')
-            ->setContent('Lorem ipsum')
-            ->setAuthor('Some one')
-            ->setCreatedAt(new \DateTime());
-         
-        dd($post);    
-
+    
+        $posts = $postRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'post' => $post
+            'posts' => $posts,
+            
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="show")
+     * @return Response a response instance
+     */
+    public function show(Post $post): Response
+    {
+        return $this->render('home/post.html.twig', [
+            'post' => $post,
+            
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Post;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +13,18 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+
+        for($i=0; $i<10; $i++){
+            $post = new Post();
+            $post->setTitle($faker->sentence(2, true))
+            ->setContent($faker->sentence(10, true))
+            ->setAuthor($faker->name())
+            ->setCreatedAt($faker->dateTimeBetween('-6 months'));
+
+        $manager->persist($post);    
+        }
 
         $manager->flush();
     }
